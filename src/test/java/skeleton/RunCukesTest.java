82de8e20812b6e5f27eb59b0bfbf949894f6e48c
smitaa94 <54269547+smitaa94@users.noreply.github.com>
2019-08-23 15:39:33 +0530

    Update RunCukesTest.java

--- src/test/java/skeleton/RunCukesTest.java
+++ src/test/java/skeleton/RunCukesTest.java
@@ -5,6 +5,6 @@ import cucumber.api.junit.Cucumber;
 import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
-@CucumberOptions(tags={"@Smoke"},plugin = {"json:target/cucumber.json"})
+@CucumberOptions(plugin = {"json:target/cucumber.json"})
 public class RunCukesTest {
 }
